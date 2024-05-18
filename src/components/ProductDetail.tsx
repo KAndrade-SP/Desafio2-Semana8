0a2a8330550db5cec843b98cbdcf7632d1b329b1
LidiaMessias <lidia.mess@gmail.com>
2024-05-18 20:28:58 -0300

    feat: implement of products-page(optional)

--- src/components/ProductDetail.tsx
+++ src/components/ProductDetail.tsx
@@ -1,8 +1,12 @@
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import plants from '../data';
+import { useNavigate } from 'react-router-dom';
+/*import plants from '../data';*/
 import Error404 from '../pages/Error404/Error404';
+import axios from 'axios';
+import Cactus from '../assets/images/img_01.png';
 
-/*interface Product {
+/*type Product = {
     id: number;
     name: string;
     subtitle: string;
@@ -15,48 +19,86 @@ import Error404 from '../pages/Error404/Error404';
     imgUrl: string;
 }*/
 
+type PlantType = {
+    id: number,
+    plantName: string,
+    plantSubtitle: string,
+    plantType: string,
+    price: number,
+    isInSale: boolean,
+    discountPercentage: number,
+    plantLabel: string,
+    features: string,
+    description: string,
+  }
+
 const ProductDetail = () => {
-    const {id} = useParams();
+    const {id} = useParams<{ id: string }>();
+    const [plant, setPlant ] = useState<PlantType | null>(null);
+   /*const [loading, setLoading] = useState(true);*/
+    const [error, setError] = useState<string | null>(null);
+    const navigate = useNavigate();
 
-    /*if (!id || isNaN(parseInt(id, 10))) {
-        <Error404/>; 
-    }*/
+    useEffect(() => {
+        if (id && !isNaN(parseInt(id, 10))) {
+            const productId = parseInt(id, 10);
 
-    const productId = parseInt(id, 10);
-    const product = plants.find((product) => product.id === productId);
+            axios.get(`http://localhost:3000/plants/${productId}`).then(response => {
+                setPlant(response.data);
+               // setLoading(false);
+            }).catch(err => {
+                setError("Produto não encontrado.");
+              //  setLoading(false);
+            });
+        } else {
+            setError("Parâmetro `id` inválido");
+            //setLoading(false);
+        }
+    }, [id]);
+
+    if (error) {
+        navigate('/error404');
+        return null;
+    }
+
+    if (!plant) {
+        return null;
+    }
+
+    
+    /*const product = plants.find((product) => product.id === productId);
 
     if (!product) {
         <Error404/>
-    }
+    }*/
 
     // Dividir as frases das features pelo ponto
-    const feature = product?.features.split('.').map(feature => feature.trim()).filter(feature => feature.length > 0);
+    const feature = plant.features.split('.').map(feature => feature.trim()).filter(feature => feature.length > 0);
  
     return (
 
-        <div className='px-8 py-14 md:px-12 md:py-16 lg:px-16 lg:py-16 bg-lightgray'>
+    <div className='px-8 py-14 md:px-12 md:py-16 lg:px-16 lg:py-16 bg-lightgray'>
         <div className='md:mx-auto lg:grid lg:grid-cols-2 lg:gap-16 md:py-10'>
           <div className='lg:w-full lg:pt-8 lg:flex lg:items-center lg:justify-center mb-8 md:mx-auto'>
-            <img src={product?.imgUrl} alt={product?.description} />
+            <img src={Cactus} alt={plant.plantName} />
           </div>
           <div className='md:mx-auto flex flex-col justify-start  2xl:max-w-2xl '>
-            <h3 className=" text-lunargreen font-ebgaramond-bold text-3xl md:pb-3 md:text-4xl lg:text-5xl lg:pb-2 2xl:text-6.5xl">{product?.name}</h3>
-            <span className=" text-lg md:py-3 lg:pt-0 lg:pb-2 font-lato-bold 2xl:text-2xl  text-darkgray" >{product?.subtitle} </span>
+            <h3 className=" text-lunargreen font-ebgaramond-bold text-3xl md:pb-3 md:text-4xl lg:text-5xl lg:pb-2 2xl:text-6.5xl">{plant.plantName}</h3>
+            <span className=" my-2 text-lg md:py-3 lg:pt-0 lg:pb-2 font-lato-bold 2xl:text-2xl  text-darkgray" >{plant.plantSubtitle} </span>
             <div>
-                {product?.label.map((label, index) => (
-                    <button key={index} className=' mr-4 lg:my-2 bg bg-whitegreen border-lightgreen border-2 rounded-3xl  w-20 h-10 text-flaggreen font-raleway-regular pointer-events-none'>{label}</button>
-                ))}
+                <button className=' mr-4 lg:my-2 bg bg-whitegreen border-lightgreen border-2 rounded-3xl  w-20 h-10 text-flaggreen font-raleway-regular pointer-events-none'>{plant.plantLabel}</button>
+                <button className=' mr-4 lg:my-2 bg bg-whitegreen border-lightgreen border-2 rounded-3xl  w-auto h-10 px-2 text-flaggreen font-raleway-regular pointer-events-none'>{plant.plantType}</button>
             </div>
-            <span className=' pt-3 lg:pt-2 font-lato-bold text-xl'>{product?.price}</span>
-            <button className='h-14 w-44 bg-lunargreen  text-almwhite font-raleway-regular my-6 lg:my-5 hover:bg-avacado hover:text-black hover:font-raleway-bold'><a href="">Check out</a></button>            
+            <span className=' pt-3 lg:pt-2 font-lato-bold text-xl'>${plant.price}</span>
+            <button className='flex-row overflow-hidden h-14 w-40 bg-lunargreen  text-almwhite font-raleway-regular my-6 lg:my-5 hover:bg-avacado  hover:font-raleway-bold transition-all'><a href="">Check out</a></button>            
             <h3 className='font-lato-bold text-xl mb-3'>Features</h3>
             <ul className='list-disc font-raleway-regular pl-4'>
-                {feature?.map((feature, index) => (
+                {feature.map((feature, index) => (
                     <li key={index}>{feature}</li>
                 ))}
             </ul>
             <h3 className='mt-6 mb-3 font-lato-bold text-xl'>Description</h3>
-            <p className='font-raleway-regular' >{product?.description} </p>
+            <p className='font-raleway-regular' >{plant.description} </p>
           </div>
         </div>
     </div>
--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -1,40 +1,82 @@
 import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import plants from '../data';
+import axios from 'axios';
+import Error404 from '../pages/Error404/Error404';
+import { useNavigate } from 'react-router-dom';
+import img01 from '../assets/images/img_01.png';
+import img02 from '../assets/images/img_02.png';
+import img03 from '../assets/images/img_03.png';
+import img04 from '../assets/images/img_04.png';
 
-interface Product {
-    id: number;
-    name: string;
-    subtitle: string;
-    label: string[];
-    price: string;
-    isInSale: boolean;
-    discountPercentage: number;
-    features: string;
-    description: string;
-    imgUrl: string;
-}
+type PlantType = {
+    id: number,
+    plantName: string,
+    plantSubtitle: string,
+    plantType: string,
+    price: number,
+    isInSale: boolean,
+    discountPercentage: number,
+    plantLabel: string,
+    features: string,
+    description: string,
+};
 
 const ProductList = () => {
 
-    const [products, setProducts] = useState<Product[]>([]);
+    const [plants, setPlants] = useState<PlantType[]>([]);
+    /*const [loading, setLoading] = useState(true);*/
+    const [error, setError] = useState<string | null>(null);
+    const navigate = useNavigate();
 
     useEffect(() => {
-        setProducts(plants);
+        /*setProducts(plants);*/
+        axios.get('http://localhost:3000/plants').then(response => {
+            setPlants(response.data);
+            //setLoading(false);
+        }).catch(err => {
+            setError('Erro ao carregar produtos');
+            //setLoading(false);
+        });
     }, []);
 
+    if (error) {
+        navigate('/error404');
+        return null;
+    }
+
+    const images = [img01, img02, img03, img04];
+    
     return (
         <div className=' bg-lightgray'>
             <h1 className='text-lunargreen font-ebgaramond-bold text-3xl md:pb-3 md:text-4xl lg:text-5xl text-center my-20 '>Our Plants for Your 
                 <span className=' text-avacado'> Green</span> Side
             </h1>
-            <ul>
-                {products.map((product) => (
-                    <li key={product.id} >
-                        <Link to={`/product/${product.id}`} >{product.name}</Link>  
-                    </li>
-                ))}
-            </ul>
+
+            <div className=' lg:grid md:grid md:grid-cols-2 lg:grid-cols-3 lg:gap-16 px-12  mb-32'>
+                {plants.map((plant, index) => {
+                    const image = images[index % images.length];
+                    return (
+                    <div className=' flex flex-col md:items-center  mb-20'>
+                        <div key={plant.id} className='flex flex-col justify-start md:w-80 lg:w-80 shadow-xl h-'>
+                            <Link to={`/product/${plant.id}`}>                   
+                                <img src={image} alt={plant.plantName} className='md:max-h-72 w-full'/>
+                                <div className='px-4'>
+                                    <h3 className=' text-lunargreen font-ebgaramond-bold text-2xl mt-4 mb-1' >{plant.plantName}</h3>
+                                    <span className='font-lato-bold text-darkgray' >{plant.plantSubtitle}</span>
+                                    <p className='font-lato-bold text-xl my-2'>${plant.price}</p>
+                                    <div>
+                                        <button className=' mr-4 bg-whitegreen border-lightgreen border-2 rounded-3xl w-20 h-10 px-2 text-flaggreen font-raleway-regular pointer-events-none'>{plant.plantLabel}</button>
+                                        <button className=' mr-4 bg-whitegreen border-lightgreen border-2 rounded-3xl w-auto h-10 px-2 text-flaggreen font-raleway-regular pointer-events-none'>{plant.plantType}</button>
+                                    </div>
+                                    {/*<p className='font-raleway-regular'>{plant.description}</p>*/}
+                                    <button className='h-14 w-40  my-4 font-raleway-regular bg-lunargreen flex-row overflow-hidden text-almwhite text-base hover:bg-avacado  hover:font-raleway-bold transition-all'>See more</button>
+                                </div>
+                            </Link>
+                        </div>
+                    </div>
+                    );
+                })}
+            </div>
         </div>
     )
 }
