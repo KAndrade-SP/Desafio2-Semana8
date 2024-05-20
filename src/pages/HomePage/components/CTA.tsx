2ae5ef3a9ee6ef2b78fa2cf5921ee0342251b50e
KAndrade-SP <kaique.viana090@gmail.com>
2024-05-20 13:23:18 -0300

    fix: fix 404 error page redirect

--- src/pages/HomePage/components/CTA.tsx
+++ src/pages/HomePage/components/CTA.tsx
@@ -19,7 +19,7 @@ export function Cta(){
                 flowers to lush indoor <br />
                 foliage, allowing you to create your very own green oasis.
               </p>
-              <Link to={'/error404'}>
+              <Link to={'*'}>
                 <button className="h-14 w-40 font-raleway-regular bg-lunargreen flex-row overflow-hidden text-almwhite text-base hover:bg-avacado  hover:font-raleway-bold transition-all hover:scale-105">
                   Shop Now
                 </button>
